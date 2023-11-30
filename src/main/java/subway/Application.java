f2111caa5344a10844df098d9580f1b0c5272fb0
zxcev <121966058+zxcev@users.noreply.github.com>
2023-11-30 16:32:46 +0900

    feat: 모든 레이어 최종 연결

--- src/main/java/subway/Application.java
+++ src/main/java/subway/Application.java
@@ -1,10 +1,40 @@
 package subway;
 
 import java.util.Scanner;
+import subway.controller.SubwayController;
+import subway.domain.Initializer;
+import subway.domain.PathFinder;
+import subway.domain.SectionRepository;
+import subway.domain.graph.StationsGraphByDistance;
+import subway.domain.graph.StationsGraphByDuration;
+import subway.view.InputView;
+import subway.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
+        Initializer.init();
+        final StationsGraphByDistance stationsGraphByDistance = new StationsGraphByDistance();
+        final StationsGraphByDuration stationsGraphByDuration = new StationsGraphByDuration();
+        final SectionRepository sectionRepository = new SectionRepository();
+        Initializer.initGraphs(stationsGraphByDistance);
+        Initializer.initGraphs(stationsGraphByDuration);
+
+        final PathFinder pathFinder = new PathFinder(
+                stationsGraphByDistance,
+                stationsGraphByDuration,
+                sectionRepository
+        );
         final Scanner scanner = new Scanner(System.in);
-        // TODO: 프로그램 구현
+        final InputView inputView = new InputView(scanner);
+        final OutputView outputView = new OutputView();
+
+        final SubwayController subwayController = new SubwayController(
+                inputView,
+                outputView,
+                pathFinder
+        );
+
+        subwayController.run();
     }
+
 }
